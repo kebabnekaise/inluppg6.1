b1819c88904f2115ef0ffc78de2445c5a78ece5b
kebabnekaise <emsi06001@utb.vaxjo.se>
2024-02-16 10:40:49 +0100

    Add project files.

---
+++ inluppg6.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace inluppg6_x
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tal1 = 1;
+            int tal2 = 2;
+            int tal3 = 3;
+            int summa = Addera(tal1, tal2, tal3);
+            Console.WriteLine(summa);
+        }
+
+        static int Addera(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+    }
+}
